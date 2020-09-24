f21c14f25eba1088683ec0392d8466044bae5c57
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2020-09-24 16:21:18 +0200

    Shayan/Islam/Leszek

---
+++ Hospital/src/com/company/Patient.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Patient {
+    private String name;
+    private String sickness;
+    boolean isSick = false;
+
+    public Patient (String name, String sickness) {
+        this.name = name;
+        this.sickness = sickness;
+    }
+
+    public Patient (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSickness(){
+        return sickness;
+    }
+
+    public void takeMedication(Medicine medicine){
+
+        if (medicine.getTreatmentName().equals(this.sickness)){
+            this.sickness = null;
+        }
+    }
+
+    public boolean isSick() {
+        isSick = sickness != null;
+        return isSick;
+    }
+}
